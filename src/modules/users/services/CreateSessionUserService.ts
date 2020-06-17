1b81d74a71b0eeabe64db8b9d4add2e21db291ef
William Moreschi <williammoreschi@gmail.com>
2020-06-16 22:17:01 -0300

    Desacoplando a dependencia do bcrypt da aplicação

--- src/modules/users/services/CreateSessionUserService.ts
+++ src/modules/users/services/CreateSessionUserService.ts
@@ -1,12 +1,12 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import authConfig from '@config/auth';
-import User from '@modules/users/infra/typeorm/entities/User';
-
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+
+import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   email: string;
@@ -23,6 +23,9 @@ class CreateSessionUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async excute({ email, password }: IRequest): Promise<IResponse> {
@@ -33,7 +36,10 @@ class CreateSessionUserService {
       throw new AppError(errorMensage, 401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
       throw new AppError(errorMensage, 401);
