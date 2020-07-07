06da9a91e4c355b17c8fded389f925b702fbb05d
William Moreschi <williammoreschi@gmail.com>
2020-07-06 22:46:40 -0300

    Variaveis de ambiente

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import 'dotenv/config';
+
 import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import { errors } from 'celebrate';
