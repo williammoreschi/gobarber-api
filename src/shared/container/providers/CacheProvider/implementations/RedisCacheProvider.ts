a9be58da39e568a24f50647d4fb80a521f0fa27e
William Moreschi <williammoreschi@gmail.com>
2020-07-15 23:12:59 -0300

    Configurando cache usando o Redis

---
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,17 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
+export default class RedisCacheProvider implements ICacheProvider {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: string): Promise<void> {}
+
+  public async recover(key: string): Promise<string> {}
+
+  public async invalidate(key: string): Promise<void>;
+}
