e278bca99ecc20b6229f00726459bcacbabefa29
William Moreschi <williammoreschi@gmail.com>
2020-07-17 21:01:32 -0300

    Criando um middleware de segurança responsável por  monitorar as requições

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import AppError from '@shared/errors/AppError';
+import rateLimiter from '@shared/infra/middleware/rateLimiter';
 import routes from '@shared/infra/http/routes';
 import uploadConfig from '@config/upload';
 
@@ -15,6 +16,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
